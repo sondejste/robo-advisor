034d75627f1d9bf85cda3547f56c78fff84cf945
sondejste <77987453+sondejste@users.noreply.github.com>
2021-03-07 22:34:55 -0500

    Latest Refresh Date

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -10,9 +10,11 @@ response = requests.get(request_url)
 #print(response.text) 
 
 parsed_response = json.loads(response.text)
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+#breakpoint()
 
-breakpoint()
-
+##Dictionary keys are "Meta Data" and "Time Series"
+#####Keys within "Meta Data" are dict_keys(['1. Information', '2. Symbol', '3. Last Refreshed', '4. Interval', '5. Output Size', '6. Time Zone'])
 
 ####
 
@@ -22,7 +24,7 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print(f"LATEST DAY: {last_refreshed}")
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
