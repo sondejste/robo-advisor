f879b9087e396115fc658a776b04704467de0612
sondejste <77987453+sondejste@users.noreply.github.com>
2021-03-07 23:05:18 -0500

    last price in USD, automatic date

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,9 +1,22 @@
 
+#SETUP
 import requests
 import json
 from datetime import datetime
 
-request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
+##USER INPUT FOR TICKER
+
+
+
+##CURRENT DATE AND TIME
+now = datetime.now()
+dt_string = now.strftime("%m/%d/%Y at %H:%M")
+
+##MAKING REQUEST/INFO GRAB
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
 
 response = requests.get(request_url)  
 #print(type(response)) #>requests.model.response
@@ -12,23 +25,27 @@ response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+latest_close = parsed_response["Time Series (Daily)"][f"{last_refreshed}"]["4. close"]
+
 #breakpoint()
 
-now = datetime.now()
-dt_string = now.strftime("%m/%d/%Y at %H:%M")
-##Dictionary keys are "Meta Data" and "Time Series"
+##Dictionary keys are "Meta Data" and "Time Series (Daily)"
 #####Keys within "Meta Data" are dict_keys(['1. Information', '2. Symbol', '3. Last Refreshed', '4. Interval', '5. Output Size', '6. Time Zone'])
 
-####
+##RECOMMENDATION
 
+
+
+##UI
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print("SELECTED SYMBOL: IBM")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print(f"REQUEST AT: {dt_string}")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
